d2c159b8ece0b9a23f7849c4a3bff1c0869c8c0b
Jean Malvessi <jeanmalvessi@gmail.com>
2026-05-01 12:32:26 -0300

    Product Catalog smoke and load test with K6

--- etc/k6/get-products.js
+++ etc/k6/get-products.js
@@ -4,39 +4,39 @@ import { sleep, check } from 'k6';
 const BASE_URL = __ENV.BASE_URL || 'http://localhost:8083';
 
 export const options = {
-  scenarios: {
-    get_products_smoke_test: {
-      executor: 'constant-vus',
-      vus: 1,
-      duration: '5s'
+    scenarios: {
+        get_products_smoke_test: {
+            executor: 'constant-vus',
+            vus: 5,
+            duration: '5s'
+        },
+        get_products_load_test: {
+            executor: 'constant-arrival-rate',
+            rate: 100,
+            timeUnit: '1s',
+            duration: '1m',
+            startTime: '5s',
+            maxVUs: 200,
+            preAllocatedVUs: 50
+        }
     },
-    get_products_load_test: {
-      executor: 'constant-arrival-rate',
-      rate: 100,
-      timeUnit: '1s',
-      duration: '1m',
-      startTime: '5s',
-      maxVUs: 200,
-      preAllocatedVUs: 50
+    /*stages: [
+      { duration: '5s', target: 100 },
+      { duration: '5s', target: 300 },
+      { duration: '5s', target: 500 },
+      { duration: '10s', target: 1000 },
+      { duration: '3s', target: 300 },
+      { duration: '3s', target: 0 },
+    ],*/
+    thresholds: {
+        http_req_duration: ['p(95) < 800'],
+        http_req_failed: ['rate < 0.01']
     }
-  },
-  /*stages: [
-    { duration: '5s', target: 100 },
-    { duration: '5s', target: 300 },
-    { duration: '5s', target: 500 },
-    { duration: '10s', target: 1000 },
-    { duration: '3s', target: 300 },
-    { duration: '3s', target: 0 },
-  ],*/
-  thresholds: {
-    http_req_duration: ['p(95) < 800'],
-    http_req_failed: ['rate < 0.01']
-  }
 };
 
-export default function() {
-  const url = `${BASE_URL}/api/v1/products`;
-  let res = http.get(url);
-  check(res, { "status is 200": (res) => res.status === 200 });
-  sleep(1);
+export default function test() {
+    const url = `${BASE_URL}/api/v1/products`;
+    let res = http.get(url);
+    check(res, { "status is 200": (res) => res.status === 200 });
+    sleep(1);
 }
